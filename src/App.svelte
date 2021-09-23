bd47752f3253b39fccc5281521e070cec683645b
Colin Bate <colin@colinbate.com>
2021-09-23 00:50:41 -0300

    Initialize

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import ModeSwitcher from './ModeSwitcher.svelte';
 	import Tailwindcss from './Tailwindcss.svelte';
-	export let name: string;
 </script>
 <style>
 	.custom-style {
@@ -11,7 +10,7 @@
 <Tailwindcss />
 <ModeSwitcher />
 <main class="p-4 mx-auto text-center max-w-xl">
-	<h1 class="uppercase text-6xl leading-normal font-thin text-svelte">Hello {name}!</h1>
+	<h1 class="uppercase text-6xl leading-normal font-thin text-svelte">Rolling Total</h1>
 	<p class="custom-style mt-[3rem]">
 		Visit the
 		<a href="https://svelte.dev/tutorial" class="text-blue-500 underline">Svelte tutorial</a>
